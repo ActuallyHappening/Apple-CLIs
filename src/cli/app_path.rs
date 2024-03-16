90304bbbcc54463dd6cc7d4ef5188a857df705f8
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-03-16 17:23:51 +1000

    refactor: extract module

---
+++ src/cli/app_path.rs
@@ -0,0 +1,57 @@
+use camino::Utf8PathBuf;
+	use color_eyre::eyre::eyre;
+	use tracing::{event, info, warn, Level};
+
+	#[derive(clap::Args, Debug)]
+	#[group(required = true, multiple = false)]
+	pub struct AppPath {
+		/// Provide an absolute path to the .app file
+		#[arg(long, long = "path", group = "app_path_group")]
+		app_path: Option<camino::Utf8PathBuf>,
+
+		/// Use glob to find the first .app file in the current directory or any subdirectories
+		#[arg(long, group = "app_path_group")]
+		glob: bool,
+	}
+
+	impl AppPath {
+		#[tracing::instrument(level = "trace", skip(self))]
+		pub fn resolve(self) -> Result<Utf8PathBuf, color_eyre::Report> {
+			let path = match self.app_path {
+				Some(p) => p,
+				None => match self.glob {
+					false => Err(eyre!(
+						"Clap should have enforced that either `app_path` or `glob` was set"
+					))?,
+					true => {
+						let matches = glob::glob("**/*.app")
+							.map_err(|err| eyre!("Error running glob: {}", err))?
+							.filter_map(|p| p.ok())
+							.filter_map(|p| Utf8PathBuf::try_from(p).ok())
+							.collect::<Vec<_>>();
+
+						if matches.len() > 1 {
+							warn!(
+								globbed = ?matches,
+								"More than one .app file found, using the first match",
+							);
+						}
+
+						match matches.first() {
+							Some(p) => {
+								info!(message = "Using the first matched .app file", "match" = ?p);
+								p.clone()
+							}
+							None => Err(eyre!(
+								"No .app files found in the current directory or any subdirectories"
+							))?,
+						}
+					}
+				},
+			};
+			if !path.exists() {
+				Err(eyre!("Provided app path does not exist: {:?}", path))?
+			}
+			Ok(path)
+		}
+	}
---
+++ src/cli/device_name.rs
@@ -0,0 +1,48 @@
+use color_eyre::eyre::eyre;
+
+	use crate::{prelude::DeviceName, xcrun::simctl::XcRunSimctlInstance};
+
+	#[derive(clap::Args, Debug)]
+	#[group(required = true, multiple = false)]
+	pub struct DeviceSimulator {
+		/// Automatically selects the latest iPad simulator
+		#[arg(long, group = "device_name")]
+		ipad: bool,
+
+		/// Automatically selects the latest iPhone simulator
+		#[arg(long, group = "device_name")]
+		iphone: bool,
+
+		/// Provide an exact name, e.g. "iPhone 12 Pro Max"
+		#[arg(group = "device_name")]
+		name: Option<DeviceName>,
+	}
+
+	impl DeviceSimulator {
+		#[tracing::instrument(level = "trace", skip(self, simctl_instance))]
+		pub fn resolve(
+			self,
+			simctl_instance: &XcRunSimctlInstance,
+		) -> Result<DeviceName, color_eyre::Report> {
+			match (self.ipad, self.iphone, self.name) {
+				(false, false, Some(name)) => Ok(name),
+				(true, false, None) => {
+					let list = simctl_instance.list()?;
+					let latest_ipad = list
+						.ipads()
+						.max()
+						.ok_or_else(|| eyre!("No simulator iPads found!"))?;
+					Ok(latest_ipad.clone().into())
+				}
+				(false, true, None) => {
+					let list_output = &simctl_instance.list()?;
+					let latest_iphone = list_output
+						.iphones()
+						.max()
+						.ok_or_else(|| eyre!("No simulator iPhones found!"))?;
+					Ok(latest_iphone.clone().into())
+				}
+				_ => Err(eyre!("Clap arguments should prevent this")),
+			}
+		}
+	}
