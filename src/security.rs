822123d3443027a84803cd6735d80639a924c82d
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-03-16 14:42:03 +1000

    todo: parse display success

--- src/security.rs
+++ src/security.rs
@@ -3,8 +3,9 @@ use crate::prelude::*;
 pub mod find_certificate;
 pub use find_certificate::Certificate;
 
+#[derive(Debug)]
 pub struct SecurityCLIInstance {
 	exec_path: Utf8PathBuf,
 }
 
-impl_exec_instance!(SecurityCLIInstance, "security");
+impl_exec_instance!(SecurityCLIInstance, "security", skip_version_check);
