d8a3838a959d330970cfd2c1a70722b86e1e4d49
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-03-16 16:32:11 +1000

    fix: compiles

--- src/codesign/display.rs
+++ src/codesign/display.rs
@@ -1,88 +1,98 @@
 use std::str::FromStr;
 
-use crate::{
-	prelude::*,
-	shared::{ws, NomFromStr},
-};
+use crate::prelude::*;
 
 use camino::Utf8Path;
-use nom::{
-	branch::alt,
-	bytes::complete::{tag, take_till, take_while},
-	combinator::{map, map_res, rest, success},
-	sequence::terminated,
-	IResult,
-};
-use serde::Serialize;
 
-use self::signed_keys::SignedKeys;
+use self::code_sign_output::CodeSignOutput;
 
 use super::CodesignCLIInstance;
 
-#[derive(Debug, Serialize)]
-pub enum CodeSignOutput {
-	NotSignedAtAll {
-		path: Utf8PathBuf,
-	},
+mod code_sign_output {
+	use std::str::FromStr;
 
-	SignedKeys(signed_keys::SignedKeys),
+	use camino::{Utf8Path, Utf8PathBuf};
+	use nom::{
+		branch::alt,
+		bytes::complete::{tag, take_till, take_while},
+		combinator::{map, map_res, rest, success},
+		sequence::terminated,
+		IResult,
+	};
+	use serde::Serialize;
+	use tracing::debug;
 
-	/// Represents a successful call to `codesign -d`
-	///
-	#[doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/docs/inline/TODO.md"))]
-	SuccessUnimplemented {
-		stdout: String,
-	},
+	use crate::{error, shared::{ws, NomFromStr}};
 
-	#[doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/docs/inline/TODO.md"))]
-	UnImplemented(String),
-}
+	use self::signed_keys::SignedKeys;
 
-mod signed_keys;
+	use super::error::Error;
 
-fn parse_key_value(input: &str) -> IResult<&str, CodeSignOutput> {
-	let (remaining, path) = map(
-		terminated(take_while(|c| c != ':'), tag(": ")),
-		Utf8Path::new,
-	)(input)?;
-	map(ws(tag("code object is not signed at all")), move |_| {
-		debug!("Parsed NotSignedAtAll");
-		CodeSignOutput::NotSignedAtAll {
-			path: path.to_owned(),
-		}
-	})(remaining)
-}
+	mod signed_keys;
 
-impl NomFromStr for CodeSignOutput {
-	fn nom_from_str(input: &str) -> nom::IResult<&str, Self> {
-		alt((
-			parse_key_value,
-			map_res(rest, |s| {
-				SignedKeys::from_raw(s).map(CodeSignOutput::SignedKeys)
-			}),
-			// map(ws(rest), |s: &str| {
-			// 	debug!(?s, "Parsed SuccessUnimplemented");
-			// 	CodeSignOutput::UnImplemented(s.to_owned())
-			// }),
-		))(input)
+	#[derive(Debug, Serialize)]
+	pub enum CodeSignOutput {
+		NotSignedAtAll {
+			path: Utf8PathBuf,
+		},
+
+		SignedKeys(signed_keys::SignedKeys),
+
+		/// Represents a successful call to `codesign -d`
+		///
+		#[doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/docs/inline/TODO.md"))]
+		SuccessUnimplemented {
+			stdout: String,
+		},
+
+		#[doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/docs/inline/TODO.md"))]
+		UnImplemented(String),
 	}
-}
 
-impl FromStr for CodeSignOutput {
-	type Err = error::Error;
+	fn parse_key_value(input: &str) -> IResult<&str, CodeSignOutput> {
+		let (remaining, path) = map(
+			terminated(take_while(|c| c != ':'), tag(": ")),
+			Utf8Path::new,
+		)(input)?;
+		map(ws(tag("code object is not signed at all")), move |_| {
+			debug!("Parsed NotSignedAtAll");
+			CodeSignOutput::NotSignedAtAll {
+				path: path.to_owned(),
+			}
+		})(remaining)
+	}
 
-	fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
-		match CodeSignOutput::nom_from_str(s) {
-			Ok(("", output)) => Ok(output),
-			Ok((remaining, output)) => Err(Error::ParsingRemainingNotEmpty {
-				input: s.to_owned(),
-				remaining: remaining.to_owned(),
-				parsed_debug: format!("{:#?}", output),
-			}),
-			Err(e) => Err(Error::ParsingFailed {
-				name: "CodeSignOutput".to_owned(),
-				err: e.to_owned(),
-			}),
+	impl NomFromStr for CodeSignOutput {
+		fn nom_from_str(input: &str) -> nom::IResult<&str, Self> {
+			alt((
+				parse_key_value,
+				map_res(rest, |s| {
+					SignedKeys::from_raw(s).map(CodeSignOutput::SignedKeys)
+				}),
+				// map(ws(rest), |s: &str| {
+				// 	debug!(?s, "Parsed SuccessUnimplemented");
+				// 	CodeSignOutput::UnImplemented(s.to_owned())
+				// }),
+			))(input)
+		}
+	}
+
+	impl FromStr for CodeSignOutput {
+		type Err = error::Error;
+
+		fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+			match CodeSignOutput::nom_from_str(s) {
+				Ok(("", output)) => Ok(output),
+				Ok((remaining, output)) => Err(Error::ParsingRemainingNotEmpty {
+					input: s.to_owned(),
+					remaining: remaining.to_owned(),
+					parsed_debug: format!("{:#?}", output),
+				}),
+				Err(e) => Err(Error::ParsingFailed {
+					name: "CodeSignOutput".to_owned(),
+					err: e.to_owned(),
+				}),
+			}
 		}
 	}
 }
